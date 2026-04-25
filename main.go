08648e3bf5248ad27643de0a0cbb84b053f8621d
James <kendrickj5@yahoo.com>
2026-04-25 00:40:42 -0500

    Remove commented-out code in main.go for cleaner implementation

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	// "fmt"
-	// "time"
-
 	cmd "github.com/jlkendrick/grimoire/cmd"
 )
 
 func main() {
-	// start_time := time.Now()
 	cmd.Execute()
-	// end_time := time.Now()
-	// duration := end_time.Sub(start_time)
-	// fmt.Printf("Command executed in %v\n", duration)
 }
